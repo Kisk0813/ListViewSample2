661b8554d439e5dc52d7889719f4e6bb421ff45d
Keisuke <16010409@anabuki-college.ac.jp>
2018-05-31 10:00:30 +0900

    finished 5

--- app/src/main/java/com/websarva/wings/android/listviewsample2/ListViewSample2Activity.java
+++ app/src/main/java/com/websarva/wings/android/listviewsample2/ListViewSample2Activity.java
@@ -1,10 +1,13 @@
-package com.websarva.wings.android.listviewsample2;
+package jp.hotmix.listviewsample2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,27 +18,36 @@ public class ListViewSample2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_sample2);
 
-        //ListViewオブジェクトを取得。
         ListView lvMenu = findViewById(R.id.lvMenu);
-        //リストビューに表示するリストデータ用Listオブジェクトを作成。
         List<String> menuList = new ArrayList<>();
-        //リストデータ登録。
+
         menuList.add("から揚げ定食");
         menuList.add("ハンバーグ定食");
         menuList.add("生姜焼き定食");
         menuList.add("ステーキ定食");
         menuList.add("野菜炒め定食");
         menuList.add("とんかつ定食");
-        menuList.add("ミンチかつ定食");
+        menuList.add("ミンチカツ定食");
         menuList.add("チキンカツ定食");
         menuList.add("コロッケ定食");
         menuList.add("焼き魚定食");
         menuList.add("焼肉定食");
-        //アダプタオブジェクトを作成
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(ListViewSample2Activity.this,
-                android.R.layout.simple_list_item_1, menuList);
-        //リストビューにアダプタオブジェクトを設定
-        lvMenu.setAdapter(adapter);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                ListViewSample2Activity.this,
+                android.R.layout.simple_list_item_1,
+                menuList);
+
+        lvMenu.setAdapter(adapter);
+        lvMenu.setOnItemClickListener(new ListItemClickListener());
+    }
+
+    private class ListItemClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            OrderConfirmDialogFragment dialogFragment = new OrderConfirmDialogFragment();
+            dialogFragment.show(getSupportFragmentManager(), "OrderConfirmDialogFragment");
+        }
     }
 }
--- app/src/main/java/com/websarva/wings/android/listviewsample2/OrderConfirmDialogFragment.java
+++ app/src/main/java/com/websarva/wings/android/listviewsample2/OrderConfirmDialogFragment.java
@@ -1,27 +1,49 @@
-package com.websarva.wings.android.listviewsample2;
+package jp.hotmix.listviewsample2;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
+import android.widget.Toast;
 
 public class OrderConfirmDialogFragment extends DialogFragment {
+    @NonNull
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState){
-        //ダイアログビルダーを生成。
-        AlertDialog.builder builder = new AlertDialog.Builder(getActivity());
-        //ダイアログのタイトルを設定。
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
         builder.setTitle(R.string.dialog_title);
-        //ダイアログのメッセージを設定。
         builder.setMessage(R.string.dialog_msg);
-        //Positive Buttonを設定。
         builder.setPositiveButton(R.string.dialog_btn_ok, new DialogButtonClickListener());
-        //Negative Buttonを設定。
         builder.setNegativeButton(R.string.dialog_btn_ng, new DialogButtonClickListener());
-        //NeutralButtonを設定。
         builder.setNeutralButton(R.string.dialog_btn_nu, new DialogButtonClickListener());
-        //ダイアログオブジェクトを生成し、リターン
-        AlertDialog dialog = builder.create();
-        return dialog;
+
+
+        AlertDialog alert = builder.create();
+        return alert;
+    }
+
+    private class DialogButtonClickListener implements DialogInterface.OnClickListener {
+
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            String msg = "";
+            switch (which){
+                case DialogInterface.BUTTON_POSITIVE:
+                    msg = getString(R.string.dialog_ok_toast);
+                    break;
+                case DialogInterface.BUTTON_NEGATIVE:
+                    msg = getString(R.string.dialog_ng_toast);
+                    break;
+
+                case DialogInterface.BUTTON_NEUTRAL:
+                    msg = getString(R.string.dialog_nu_toast);
+                    break;
+            }
+
+            Toast.makeText(getActivity(), msg, Toast.LENGTH_LONG).show();
+        }
     }
 }
